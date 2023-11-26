05961f6aa5bbf7c215cef94564c6ea8168cb5501
ziad elsaadany <ziadelsaadany73@gmail.com>
2023-11-26 13:53:31 +0200

    location package added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const GPS());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class GPS extends StatelessWidget {
+  const GPS({super.key});
 
   // This widget is the root of your application.
   @override
