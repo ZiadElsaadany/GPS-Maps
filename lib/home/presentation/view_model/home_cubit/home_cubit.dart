167e457bdaa5e54ee90f1c698c9fe2c5ac014c18
ziad elsaadany <ziadelsaadany73@gmail.com>
2023-11-27 17:43:35 +0200

    add Home cubit

---
+++ lib/home/presentation/view_model/home_cubit/home_cubit.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gps/home/presentation/view_model/home_cubit/home_states.dart';
+import 'package:location/location.dart';
+
+class HomeCubit extends Cubit<HomeStates> {
+  HomeCubit() : super(HomeInitialState());
+
+
+  late PermissionStatus permissionStatus;
+
+  bool serviceEnabled= false;
+
+  Location location =  Location();
+
+  //to get permission that allow user to get permission
+  Future<bool> isPermissionGranted( ) async{
+    permissionStatus=await location.hasPermission();
+    if(permissionStatus== PermissionStatus.denied) {
+      // not allow using location
+      //request to permission from user to use location
+      permissionStatus = await location.requestPermission();
+    }
+    return permissionStatus == PermissionStatus.granted;
+  }
+
+// no internet and gps
+  Future<bool> isServiceEnabled ( ) async {
+    serviceEnabled =await location.serviceEnabled();
+    if(!serviceEnabled) {
+      // to open the service  --> gps
+      serviceEnabled = await location.requestService();
+    }
+    return serviceEnabled ;
+  }
+
+// get location
+  LocationData ? locationData ;
+
+  getUserLocation( )async {
+    bool granted=   await isPermissionGranted();
+    if(!granted) {
+      // user denied permission
+      return ;
+    }
+    bool gpsEnabled =  await isServiceEnabled();
+    if(!gpsEnabled){
+      // user didn't allow to open GPS
+      return;
+    }
+    locationData =     await location.getLocation();
+    if (kDebugMode) {
+      print(locationData?.latitude.toString());
+    }
+    if (kDebugMode) {
+      print(locationData?.longitude.toString());
+    }
+
+
+
+
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gps/home/presentation/view/home_screen.dart';
+import 'package:gps/home/presentation/view_model/home_cubit/home_cubit.dart';
 import 'package:location/location.dart';
 
 void main() {
@@ -20,7 +23,7 @@ class _GPSState extends State<GPS> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    getUserLocation();
+    BlocProvider.of<HomeCubit>(context).getUserLocation();
   }
 
   // This widget is the root of your application.
@@ -54,52 +57,5 @@ class _GPSState extends State<GPS> {
     );
   }
 
-  late PermissionStatus permissionStatus;
 
-   bool serviceEnabled= false;
-
-  Location location =  Location();
-
-  //to get permission that allow user to get permission
-Future<bool> isPermissionGranted( ) async{
-  permissionStatus=await location.hasPermission();
-if(permissionStatus== PermissionStatus.denied) {
-  // not allow using location
-  //request to permission from user to use location
-   permissionStatus = await location.requestPermission();
-}
-return permissionStatus == PermissionStatus.granted;
-}
-
-// no internet and gps
-Future<bool> isServiceEnabled ( ) async {
-  serviceEnabled =await location.serviceEnabled();
-if(!serviceEnabled) {
-  // to open the service  --> gps
- serviceEnabled = await location.requestService();
-}
-return serviceEnabled ;
-}
-
-// get location
-  LocationData ? locationData ;
-
-getUserLocation( )async {
-bool granted=   await isPermissionGranted();
-if(!granted) {
-  // user denied permission
-  return ;
-}
- bool gpsEnabled =  await isServiceEnabled();
-if(!gpsEnabled){
-  // user didn't allow to open GPS
-  return;
-}
-locationData =     await location.getLocation();
-print(locationData?.latitude.toString());
-print(locationData?.longitude.toString());
-
-
-
-}
 }
